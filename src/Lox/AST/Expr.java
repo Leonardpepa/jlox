60905829a4eadd854e074c5f47a7a2d18a983d8e
DESKTOP-J7GS40K\USER <leopepp4@gmail.com>
2022-08-04 01:56:20 +0300

    added base class for expr

---
+++ src/Lox/AST/Expr.java
@@ -0,0 +1,17 @@
+package Lox.AST;
+
+import Lox.Scanner.Token;
+
+abstract class Expr {
+    static class Binary extends Expr {
+        Binary(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+        final Expr left;
+        final Token operator;
+        final Expr right;
+    }
+    // Other expressions...
+}
