95ca6c092057de22e65cb7769a11780cebe16d7a
DESKTOP-J7GS40K\USER <leopepp4@gmail.com>
2022-08-06 22:30:21 +0300

    created interpreter class with visitor pattern to evaluate expressions

---
+++ src/Lox/Interpreter/Interpreter.java
@@ -0,0 +1,120 @@
+package Lox.Interpreter;
+
+import Lox.AST.EXPRESSION.*;
+
+class Interpreter implements Expr.Visitor<Object> {
+    @Override
+    public Object visitAssignExpr(Assign expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitBinaryExpr(Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+        switch (expr.operator.type) {
+            case MINUS:
+                return (double)left - (double)right;
+            case SLASH:
+                return (double)left / (double)right;
+            case STAR:
+                return (double)left * (double)right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left + (double)right;
+                }
+                if (left instanceof String && right instanceof String) {
+                    return (String)left + (String)right;
+                }
+                break;
+            // comparison
+            case GREATER:
+                return (double)left > (double)right;
+            case GREATER_EQUAL:
+                return (double)left >= (double)right;
+            case LESS:
+                return (double)left < (double)right;
+            case LESS_EQUAL:
+                return (double)left <= (double)right;
+            // equality
+            case BANG_EQUAL: return !isEqual(left, right);
+            case EQUAL_EQUAL: return isEqual(left, right);
+        }
+        // Unreachable.
+        return null;
+    }
+
+    @Override
+    public Object visitCallExpr(Call expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitGetExpr(Get expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    @Override
+    public Object visitLiteralExpr(Literal expr) {
+        return expr.value;
+    }
+
+    @Override
+    public Object visitLogicalExpr(Expr expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitSetExpr(Expr expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitSuperExpr(Super expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitThisExpr(This expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitUnaryExpr(Unary expr) {
+        Object right = evaluate(expr.right);
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                return -(double)right;
+        }
+        // Unreachable.
+        return null;
+    }
+
+    @Override
+    public Object visitVariableExpr(Variable expr) {
+        return null;
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null) return false;
+        if (object instanceof Boolean) return (boolean)object;
+        return true;
+    }
+
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null) return true;
+        if (a == null) return false;
+        return a.equals(b);
+    }
+}
